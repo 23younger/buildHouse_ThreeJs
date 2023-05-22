31c392abab7868976b6f8fb773222a665a7d1d8c
童洋 <tongyang@tongyangdeAir.lan>
2023-05-22 23:07:49 +0800

    feat: 尝试加载模型搭房子

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,49 @@
 <template>
-  <div class="current-page">
-    <canvas
-      id="draw"
-      style="border: 1px solid; background-color: #000;"
-    ></canvas>
-  </div>
+  <canvas id="draw"></canvas>
 </template>
 
 <script setup>
-  import * as THREE from 'three';
-  import { onMounted } from 'vue';
-  
-  // canvas大小
-  const width = 500;
-  const height = 500;
-  // 建筑的长宽
-  const baseWidth = 40;
-  const baseLength = 60;
+import * as THREE from "three";
+import { onMounted } from "vue";
+import cao from './assets/cao.jpeg';
 
-  let scene = null; // 场景
-  let camera = null; // 相机
-  let canvas = null; // 用作渲染器的canvas
-  let renderer = null; // 渲染器
+onMounted(() => {
+  let canvas = document.querySelector("#draw"); // 用作渲染器的canvas
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  let scene = new THREE.Scene(); // 场景
+
+  let camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 1500); // 相机
+  camera.position.set(8, 1, 12);
+  scene.add(camera);
+
+  const floor = new THREE.PlaneGeometry(1500, 1500);
+  const textureLoader = new THREE.TextureLoader();
+  
+  const floorTexture = textureLoader.load(cao);
+  floorTexture.repeat.set(1500, 1500)
+  const floorMaterial = new THREE.MeshBasicMaterial({ map: floorTexture });
+  const floorMesh = new THREE.Mesh(floor, floorMaterial);
+  scene.add(floorMesh);
+
+  // 灯光
+  const light = new THREE.AmbientLight(0xffffff, 3); // 光照颜色，光照强度
+  scene.add(light);
+
+  const renderer = new THREE.WebGL1Renderer({
+    canvas, // 渲染器绘制其输出的canvas
+    antialias: true, // 是否抗锯齿
+    alpha: true, // canvas是否包含alpha-透明度，默认为false
+  }); // 渲染器
+  // 设置渲染器大小，这里设置为整个窗口，会自动设置canvas的大小
+  renderer.setSize(800, 600);
+  render();
+
+  function render() {
+    renderer.render(scene, camera);
+  }
+});
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
